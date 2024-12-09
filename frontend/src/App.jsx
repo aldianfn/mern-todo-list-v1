9f9703fc8b97387177fc3b63d565ddf180c05340
aldianfn <aldian.1998@gmail.com>
2024-12-09 13:44:58 +0700

    Initiate react vite and chakra ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/react'
+
+function App() {
+  
+  return (
+    <>
+      <Button colorScheme='red' size='xs'>
+        Button
+      </Button>
+    </>
+  )
+}
+
+export default App
